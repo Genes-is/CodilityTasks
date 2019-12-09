c6044e3bc8a67b783ab4d5c1e055d4783fdbc1c7
Hennadii <genagorbachov1@gmail.com>
2019-12-09 14:11:56 +0200

    Slice module solved

--- Codility/Painless/MaxSliceSum.cs
+++ Codility/Painless/MaxSliceSum.cs
@@ -6,5 +6,16 @@ namespace Codility.Painless
 {
     class MaxSliceSum
     {
+        public int solution(int[] A)
+        {
+            int cur = A[0];
+            int max = A[0];
+            for (int i = 1; i < A.Length; i++)
+            {
+                cur = Math.Max(A[i], cur + A[i]);
+                max = Math.Max(cur, max);
+            }
+            return max;
+        }
     }
 }
