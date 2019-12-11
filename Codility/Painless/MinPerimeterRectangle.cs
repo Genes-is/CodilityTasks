f9c5ecd006206bf947612d1e7e86473cffb96f42
Hennadii <genagorbachov1@gmail.com>
2019-12-11 13:39:59 +0200

    Prime and composite numbers module solved

---
+++ Codility/Painless/MinPerimeterRectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Codility.Painless
+{
+    class MinPerimeterRectangle
+    {
+        public int solution(int N)
+        {
+            int sqrt = (int)Math.Sqrt(N);
+            for (int i = sqrt; i > 0; i--)
+                if (N % i == 0)
+                    return 2 * (N / i + i);
+            return -1;
+        }
+    }
+}
---
+++ Codility/Respectable/Flags.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Codility.Respectable
+{
+    class Flags
+    {
+        public int solutionBool(int[] A)
+        {
+            int n = A.Length;
+            bool[] peaks = new bool[n];
+            int count = 0;
+
+            for (int i = 1; i < n - 1; i++)
+                if (A[i] > A[i - 1] && A[i] > A[i + 1])
+                {
+                    peaks[i] = true;
+                    count++;
+                }
+
+            if (count == 0)
+                return 0;
+
+            int sum;
+            for (int i = Math.Min(count, (int)Math.Sqrt(n)+1); i >= 0; i--) 
+            {
+                sum = 0;
+                for (int j = 1; j < n - 1; j++)
+                {
+                    if (peaks[j])
+                    {
+                        sum++;
+                        j += i - 1;
+                    }
+                }
+                if (sum >= i)
+                    return i;
+            }
+
+            return 1;
+        }
+    }
+}
