0e743e4058830ec4814229c8ea01ccaa01bd1d8b
Hennadii <genagorbachov1@gmail.com>
2019-12-13 13:49:37 +0200

    Euclidean algorithm module solved

---
+++ Codility/Respectable/ChocolatesByNumbers.cs
@@ -0,0 +1,24 @@
+﻿namespace Codility.Respectable
+{
+    class ChocolatesByNumbers
+    {
+        public int solution(int N, int M) => N / GCD(N, M);
+
+        private int GCD(int a, int b)
+        {
+            if (b == 0) return a;
+            return GCD(b, a % b);
+        }
+
+        //private int Gcd(int a, int b)
+        //{
+        //    if (a == b)
+        //        return a;
+        //    if (a > b)
+        //        Gcd(a - b, b);
+        //    else
+        //        Gcd(a, b - a);
+        //    return -1;
+        //}
+    }
+}
---
+++ Codility/Respectable/CommonPrimeDivisors.cs
@@ -0,0 +1,30 @@
+﻿namespace Codility.Respectable
+{
+    class CommonPrimeDivisors
+    {
+        private int GCD(int a, int b)
+        {
+            if (b == 0) return a;
+            return GCD(b, a % b);
+        }
+
+        public int solution(int[] A, int[] B)
+        {
+            int a, b, c, d,
+                count = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                a = A[i];
+                b = B[i];
+                d = GCD(a, b);
+                while ((c = GCD(a, d)) != 1)
+                    a /= c;
+                while ((c = GCD(b, d)) != 1)
+                    b /= c;
+                if (a == 1 && b == 1)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
